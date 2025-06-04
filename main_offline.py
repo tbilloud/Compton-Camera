6aec49740448f75790e67ec1a3734d670745ccfc
tbilloud <tbilloud@gmail.com>
2025-06-04 16:00:56 +0200

    update offline example with measured data

--- main_offline.py
+++ main_offline.py
@@ -2,8 +2,59 @@
 # Data can be measured or from Gate/Allpix simulation
 # Can be used with an offline venv (and should be on MacOS): see README.md
 
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import logging
+
+from tools.analysis_pixelHits import pixet2pixelHit
+
+try:
+    from opengate.logger import global_log
+    global_log.setLevel(logging.DEBUG)
+except ImportError:
+    global_log = logging.getLogger("dummy")
+    global_log.addHandler(logging.NullHandler())
+
 # ===========================
-# ==   INPUT               ==
+# ==   INPUT PIXEL HITS    ==
+# ===========================
+
+# SIMULATION
+pixelHits = pd.read_csv('output/pixelHits_250kBq_10ms.csv')
+plt.hist(pixelHits['Energy (keV)'], bins=240, range=(10, 250))
+plt.xlabel('Energy (keV)')
+plt.ylabel('Count')
+plt.title('Pixel Hits SIMULATED')
+plt.show()
+
+# MEASUREMENT
+file_t3pa = '/home/billoud/DATA/data-driven/Lu177_300s.t3pa'
+file_xml = '/home/billoud/workspace/MISC/MINIPIX/Calibration/MiniPIX-H06-W0130.xml'
+pixelHits_meas = pixet2pixelHit(file_t3pa, file_xml, 'H06-W0130', max_rows=1000)
+plt.hist(pixelHits_meas['Energy (keV)'], bins=240, range=(10, 250))
+plt.xlabel('Energy (keV)')
+plt.ylabel('Count')
+plt.title('Pixel Hits MEASURED')
+plt.show()
+
+sys.exit()
+
+# ===========================
+# ==   PIXEL CLUSTERS      ==
+# ===========================
+from tools.analysis_pixelClusters import pixelHits2pixelClusters
+pixelClusters = pixelHits2pixelClusters(pixelHits, npix=256, window_ns=100, f='m2')
+plt.hist(pixelClusters['Energy (keV)'], bins=240, range=(10, 250))
+plt.xlabel('Energy (keV)')
+plt.ylabel('Count')
+plt.title('Pixel Clusters')
+plt.show()
+
+sys.exit()
+
+# ===========================
+# ==   INPUT CONES         ==
 # ===========================
 
 # Simulation or experiment parameters
@@ -16,7 +67,6 @@ sensortranslation = [0 , 0 , 5 ]
 vp, vs = 0.1, (256, 256, 256)
 
 # Read cones from CSV file
-import pandas as pd
 cones = pd.read_csv('output/cones_comb.csv')
 print(cones)
 
